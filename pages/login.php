2e058e3e791a823cb1e5cccd6a4d5cca2dd3f714
Steel <alexandre0312@orange.fr>
2024-05-26 23:16:35 -0400

    update

--- pages/login.php
+++ pages/login.php
@@ -10,6 +10,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt = $pdo->prepare("SELECT * FROM users WHERE username = ?");
     $stmt->execute([$username]);
     $user = $stmt->fetch();
+
     if ($user && password_verify($password, $user['password'])) {
         $_SESSION['username'] = $user['username'];
         $_SESSION['isLogged'] = true;
